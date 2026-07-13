81e0569970bb69dc6e77f32267953eb1f3deb9be
kun20101922 <ceakhunkh@gmail.com>
2026-07-14 06:08:23 +0700

    neovim config

--- .config/nvim/lua/plugins/auto-pairs.lua
+++
@@ -1,9 +0,0 @@
-local add = MiniDeps.add
-add({
-	source = 'windwp/nvim-autopairs'
-})
-
-require('nvim-autopairs').setup({
-  enable_check_bracket_line = true
-})
-
--- .config/nvim/lua/plugins/minipairs.lua
+++
@@ -1,7 +0,0 @@
-local add = MiniDeps.add
-add({
-	source = "nvim-mini/mini.pairs",
-	checkou = "stable",
-})
-
-require("mini.pairs").setup()
